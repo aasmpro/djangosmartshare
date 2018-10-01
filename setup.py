d4ae2c18fa7734deccc39ca7f668e908a98c1fd7
Abolfazl Amiri <aa.smpro@gmail.com>
2018-10-02 00:26:18 +0330

    preparing for packaging

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smartshare",
+    version="0.8.5",
+    author="Abolfazl Amiri",
+    author_email="aa.smpro@gmail.com",
+    description="simple django app for sharing files over http / https.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aasmpro/smartshare",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
