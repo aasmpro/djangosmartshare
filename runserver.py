d2f7abb6e21e8669b3c6923f35cea2a6c5c49730
Abolfazl Amiri <aa.smpro@gmail.com>
2018-10-01 23:26:53 +0330

    creating reusable app

--- runserver.py
+++
@@ -1,8 +0,0 @@
-import socket, subprocess
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-host = socket.gethostbyname(socket.gethostname())
-port = '8000'
-print('running server on {}:{}'.format(host, port))
-subprocess.run('python3 {}/manage.py runserver {}:{}'.format(dir_path, host, port).split())
--- share/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShareConfig(AppConfig):
-    name = 'share'
