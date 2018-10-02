5aa896ad1b3a15f02c00af929082e0fabe82d1e3
Abolfazl Amiri <aa.smpro@gmail.com>
2018-10-02 12:46:48 +0330

    updating setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aasmpro/djangosmartshare",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['smartshareserver']),
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
