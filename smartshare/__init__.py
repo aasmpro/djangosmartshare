3cd28748d89675ae9a053799590a8cb0ab9e0ac4
Abolfazl Amiri <aa.smpro@gmail.com>
2018-10-02 08:08:00 +0330

    updating

--- smartshare/__init__.py
+++ smartshare/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django smart share'
-__version__ = '0.8.5'
+__version__ = '0.8.6'
 __author__ = 'Abolfazl Amiri'
 __author_email__ = 'aa.smpro@gmail.com'
 __license__ = 'GNU General Public License v3 (GPLv3)'
